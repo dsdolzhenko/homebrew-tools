a6420183c094063042489e215c0d4e06cf8f3599
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2026-03-02 16:53:37 +0100

    Update youtrack-cli version

--- Formula/youtrack-cli.rb
+++ Formula/youtrack-cli.rb
@@ -3,28 +3,28 @@
 class YoutrackCli < Formula
   desc "CLI utility to access YouTrack that is designed to be used both by humans and LLMs"
   homepage "https://github.com/dsdolzhenko/youtrack-cli"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.1.0/yt-darwin-arm64.tar.gz"
-      sha256 "5bd48273a9c4b353d65868af11b28e9ef4e9dfe1d00cc7849f51ff6e66b5d6fa"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-darwin-arm64.tar.gz"
+      sha256 "57234361e923c0430301ba4bd11c59726e669b8478058ef6fad8d6ea6eb4655a"
     end
     on_intel do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.1.0/yt-darwin-amd64.tar.gz"
-      sha256 "e6ccad4235814ce9c38f786ab1ad5249724d41073a1de45da2544909f07b6a97"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-darwin-amd64.tar.gz"
+      sha256 "3c14970a903b1ab65b6a179a2f132fef983a3876662cf8737e411014107c6f19"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.1.0/yt-linux-arm64.tar.gz"
-      sha256 "3c6cc85ec1d221d888b708f53745e33220a3d518ef070ad3ad9bdaa4556d1c08"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-linux-arm64.tar.gz"
+      sha256 "653fba7e50ca623d9ad5f560d774b869c917aee4d5eb62ffaad20459d90a5d77"
     end
     on_intel do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.1.0/yt-linux-amd64.tar.gz"
-      sha256 "81804f61e4c158945998bc688d66bfc8e39dbfba483b33fa66d0545cedfa3e78"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-linux-amd64.tar.gz"
+      sha256 "36e2c3d3905da391188fe56c308e4d64d2e4429a79b23ac73795989eb2c1d8c2"
     end
   end
 
