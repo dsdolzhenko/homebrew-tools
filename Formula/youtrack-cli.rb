d589f37af1736849c54290b896f886df2733bd3c
Dmitry Dolzhenko <mailbox@dolzhenko.me>
2026-03-13 14:17:39 +0100

    Update youtrack-cli version

--- Formula/youtrack-cli.rb
+++ Formula/youtrack-cli.rb
@@ -3,28 +3,28 @@
 class YoutrackCli < Formula
   desc "CLI to access YouTrack that is designed to be used both by humans and LLMs"
   homepage "https://github.com/dsdolzhenko/youtrack-cli"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-darwin-arm64.tar.gz"
-      sha256 "57234361e923c0430301ba4bd11c59726e669b8478058ef6fad8d6ea6eb4655a"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.3.0/yt-darwin-arm64.tar.gz"
+      sha256 "79f65c94b31bbd35b9d226487e34a84ac5835ac89b371970cf469891272ed26a"
     end
     on_intel do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-darwin-amd64.tar.gz"
-      sha256 "3c14970a903b1ab65b6a179a2f132fef983a3876662cf8737e411014107c6f19"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.3.0/yt-darwin-amd64.tar.gz"
+      sha256 "7278afeb9dbccbbc95ba7777b212d2d8ccfbbbb2016420d5a0965903e305fa07"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-linux-arm64.tar.gz"
-      sha256 "653fba7e50ca623d9ad5f560d774b869c917aee4d5eb62ffaad20459d90a5d77"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.3.0/yt-linux-arm64.tar.gz"
+      sha256 "bb21753a7e74058462eb8943a6f7d3a16d16bcff17d92db40df4dd7fa293f23f"
     end
     on_intel do
-      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.2.0/yt-linux-amd64.tar.gz"
-      sha256 "36e2c3d3905da391188fe56c308e4d64d2e4429a79b23ac73795989eb2c1d8c2"
+      url "https://github.com/dsdolzhenko/youtrack-cli/releases/download/v0.3.0/yt-linux-amd64.tar.gz"
+      sha256 "27884001eacdb8dc41585d842317cdc44fe02bb620c3b9419a80e826f15f30f3"
     end
   end
 
